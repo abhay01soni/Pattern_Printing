193a74301516788e8b2e64fdbbabfcb208813ce5
abhay01soni <abhaysoni9455@gmail.com>
2025-11-24 20:22:05 +0530

    Add files via upload

---
+++ Stage1/Main.java
@@ -0,0 +1,22 @@
+package Stage1;
+
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Q: Simple pattern print
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        for (int i = 1 ; i<=row ; i++){
+            for(int j = 1 ; j<=col ;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
---
+++ Stage1/NumberSquare.java
@@ -0,0 +1,19 @@
+package Stage1;//1 2 3 4
+//1 2 3 4
+//1 2 3 4
+//1 2 3 4
+
+import java.util.Scanner;
+
+public class NumberSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i =1; i<=n; i++){
+            for(int j = 1; j<=n; j++){
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+    }
+}
