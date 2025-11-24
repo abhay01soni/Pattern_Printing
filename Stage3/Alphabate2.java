03a386ba50a1677324d86884351d830464dc5701
abhay01soni <abhaysoni9455@gmail.com>
2025-11-24 20:22:29 +0530

    Add files via upload

---
+++ Stage3/Alphabate2.java
@@ -0,0 +1,16 @@
+package Stage3;
+
+import java.util.Scanner;
+
+public class Alphabate2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=n ; i++){
+            for(int j = 1 ; j<=(n+1)-i ; j++){
+                System.out.print((char)(i + 96) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Stage3/StarTriangleHorizontalllyFlipped.java
@@ -0,0 +1,35 @@
+package Stage3;
+/*
+
+ * * * *
+ * * *
+ * *
+ *
+
+ */
+import java.util.Scanner;
+
+public class StarTriangleHorizontalllyFlipped {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=n ; i++){
+            for(int j = 1 ; j<=(n+1)-i ; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+/*
+ int n = sc.nextInt();
+ int a = n;
+        for(int i = 1; i<=n ; i++){
+            for(int j = 1 ; j<=a ; j++){
+                System.out.print("* ");
+            }
+            a--
+            System.out.println();
+        }
+ */
